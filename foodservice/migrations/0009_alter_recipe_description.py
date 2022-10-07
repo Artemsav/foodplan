6231cc64a9a28f820769020c3d29cf32611610fa
VasilyKV <103107192+VasilyKV@users.noreply.github.com>
2022-10-07 10:58:40 +0400

    media_gitignore

--- foodservice/migrations/0009_alter_recipe_description.py
+++ foodservice/migrations/0009_alter_recipe_description.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('foodservice', '0006_alter_recipe_description'),
+        ('foodservice', '0006_alter_recipeingredient_options'),
     ]
 
     operations = [
